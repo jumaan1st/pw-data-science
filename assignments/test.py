ef60bd150e3ef1131d0963a359d97fd6b6cde802
jumaan1st <mohammedjumaan.mf@gmail.com>
2025-09-18 22:26:01 +0530

    assignment done

--- assignments/test.py
+++ assignments/test.py
@@ -1,14 +1,4 @@
-from memory_profiler import profile
-
-@profile
-def create_list():
-    # Create a large list to see memory usage
-    data = [i for i in range(1000000)]
-    print("List created with", len(data), "elements.")
-
-@profile
-def main():
-    create_list()
-
-if __name__ == "__main__":
-    main()
+from bokeh.plotting import figure, show
+p = figure()
+p.line([1, 2, 3], [4, 5, 6])
+show(p)
